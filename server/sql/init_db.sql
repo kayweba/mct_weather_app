ba3a2be4791a403d644d9d19e8bb46655cc37212
Зюков Александр Вадимович <zyukov_av@automiq.local>
2025-03-04 17:35:20 +0300

    [server] Обновлен скрипт создания БЖ

--- server/sql/init_db.sql
+++ server/sql/init_db.sql
@@ -28,30 +28,40 @@ VALUES
 	(6, "Северо-западный"),
 	(7, "Северо-восточный")
 	
-CREATE TABLE measures (
-	Measure_date INTEGER NOT NULL PRIMARY KEY,
-	Morning_temperature REAL DEFAULT NULL,
-	Morning_pressure REAL DEFAULT NULL,
-	Morning_wind_speed REAL DEFAULT NULL,
-	Morning_wind_directionId INTEGER DEFAULT NULL REFERENCES windDirections(Id) ON UPDATE CASCADE ON DELETE SET NULL,
-	Morning_precipitation_typeId INTEGER DEFAULT NULL REFERENCES precipitationTypes(Id) ON UPDATE CASCADE ON DELETE SET NULL,
-	Afternoon_temperature REAL DEFAULT NULL,
-	Afternoon_pressure REAL DEFAULT NULL,
-	Afternoon_wind_speed REAL DEFAULT NULL,
-	Afternoon_wind_directionId INTEGER DEFAULT NULL REFERENCES windDirections(Id) ON UPDATE CASCADE ON DELETE SET NULL,
-	Afternoon_precipitation_typeId INTEGER DEFAULT NULL REFERENCES precipitationTypes(Id) ON UPDATE CASCADE ON DELETE SET NULL,
-	Evening_temperature REAL DEFAULT NULL,
-	Evening_pressure REAL DEFAULT NULL,
-	Evening_wind_speed REAL DEFAULT NULL,
-	Evening_wind_directionId INTEGER DEFAULT NULL REFERENCES windDirections(Id) ON UPDATE CASCADE ON DELETE SET NULL,
-	Evening_precipitation_typeId INTEGER DEFAULT NULL REFERENCES precipitationTypes(Id) ON UPDATE CASCADE ON DELETE SET NULL
+CREATE TABLE dayParts (
+	Id INTEGER NOT NULL PRIMARY KEY,
+	Description TEXT(255) NOT NULL
 )
 
-INSERT INTO measures(Measure_date, Morning_temperature, Morning_pressure, Morning_wind_speed, Morning_wind_directionId, Morning_precipitation_typeId,
-					Afternoon_temperature, Afternoon_pressure, Afternoon_wind_speed, Afternoon_wind_directionId, Afternoon_precipitation_typeId,
-					Evening_temperature, Evening_pressure, Evening_wind_speed, Evening_wind_directionId, Evening_precipitation_typeId)
+INSERT INTO dayParts (Id, Description)
+VALUES
+	(0, "Утро"),
+	(1, "День"),
+	(2, "Вечер")
+	
+CREATE TABLE measures (
+	Measure_date INTEGER NOT NULL,
+	Measure_day_part INTEGER NOT NULL,
+	Temperature REAL DEFAULT NULL,
+	Pressure REAL DEFAULT NULL,
+	Wind_speed REAL DEFAULT NULL,
+	Wind_directionId INTEGER DEFAULT NULL,
+	Precipitation_typeId INTEGER DEFAULT NULL,
+	PRIMARY KEY (Measure_date, Measure_day_part),
+	CONSTRAINT FK_Wind_direction FOREIGN KEY (Wind_directionId) REFERENCES windDirections(Id) ON UPDATE CASCADE ON DELETE SET NULL,
+	CONSTRAINT FK_Precipitation_type FOREIGN KEY (Precipitation_typeId) REFERENCES precipitationTypes(Id) ON UPDATE CASCADE ON DELETE SET NULL,
+	CONSTRAINT FK_Day_Part FOREIGN KEY (Measure_day_part) REFERENCES dayParts(Id) ON UPDATE CASCADE ON DELETE CASCADE
+)
+
+INSERT INTO measures (Measure_date, Measure_day_part, Temperature, Pressure, Wind_speed, Wind_directionId, Precipitation_typeId)
 VALUES 
-	(1739207223, 10, 747, 3, 0, 0, 15, 750, 2, 1, 1, 11, 751, 6, 2, 1),
-	(1739293623, 15, 743, 2, 3, 3, 18, 755, 7, 4, 4, 14, 756, 4, 5, 5),
-	(1739380023, 15, 743, 2, 0, 0, 3, 755, 7, 7, 1, 10, 756, 4, 0, 2),
-	(1740960000, 22, 750, 2, 0, 0, null, null, null, null, null, null, null, null, null, null)
+	(1739207223, 0, 10, 747, 3, 0, 0),
+	(1739207223, 1, 15, 750, 2, 1, 1),
+	(1739207223, 2, 11, 751, 6, 2, 1),
+	(1739293623, 0, 15, 743, 2, 3, 3),
+	(1739293623, 1, 18, 755, 7, 4, 4), 
+	(1739293623, 2, 14, 756, 4, 5, 5),
+	(1739380023, 0, 15, 743, 2, 0, 0), 
+	(1739380023, 1, 3, 755, 7, 7, 1), 
+	(1739380023, 2, 10, 756, 4, 0, 2),
+	(1740960000, 0, 22, 750, 2, 0, 0)
